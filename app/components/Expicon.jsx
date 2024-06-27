22e92b0083153a6470a50704ebb63f387b5664d2
jason-zhao-git <zjq233333@gmail.com>
2024-06-27 05:21:15 -0700

    ready for deploy test2

--- app/components/Expicon.jsx
+++ app/components/Expicon.jsx
@@ -1,6 +1,6 @@
 "use client";
 import React from "react";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 import styles from "./ex_styles/glow-text.css";
 import { useEffect } from "react";
 
@@ -10,9 +10,7 @@ const Expicon = ({ reference }) => {
     offset: ["center end", "center center"],
   });
 
-  useEffect(() => {
-    console.log('Expicon rendered with reference:', reference.current);
-  }, [reference]);
+  const pathLength = useTransform(scrollYProgress, [0, 1], [0, 1]);
 
   return (
     <figure className="absolute left-0 stroke-white">
@@ -28,7 +26,7 @@ const Expicon = ({ reference }) => {
           cy="50"
           r="20"
           className=" stroke-[5px] fill-light"
-          style={{ pathLength: scrollYProgress }}
+          style={{ pathLength }}
         />
         <circle
           cx="75"
