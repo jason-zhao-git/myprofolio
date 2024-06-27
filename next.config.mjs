b87f021766390e202a9801613e9fd0d4870c5d05
jason-zhao-git <zjq233333@gmail.com>
2024-06-27 05:42:38 -0700

    ready for deploy test3

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    webpack(config, { isServer }) {
-      config.module.rules.push({
-        test: /\.(mp3|wav|ogg|flac|aac)$/,
-        use: {
-          loader: 'file-loader',
-          options: {
-            name: '[name].[hash].[ext]',
-            outputPath: 'static/media/',
-            publicPath: '/_next/static/media/',
-          },
+  webpack(config, { isServer }) {
+    config.module.rules.push({
+      test: /\.(mp3|wav|ogg|flac|aac)$/,
+      use: {
+        loader: "file-loader",
+        options: {
+          name: "[name].[hash].[ext]",
+          outputPath: "static/media/",
+          publicPath: "/_next/static/media/",
         },
-      });
-  
-      return config;
-    },
-  };
-  
-  export default nextConfig;
+      },
+    });
+
+    return config;
+  },
+};
+
+export default nextConfig;
